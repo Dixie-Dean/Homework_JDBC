e842878d92c462336c6e2a3ae3c9d950325b01f6
Dixie Dean <d.w.sokolov26@gmail.com>
2023-10-30 16:55:14 +0300

    Created Repository

---
+++ src/main/java/com/example/homework_jdbc/repository/MyRepository.java
@@ -0,0 +1,39 @@
+package com.example.homework_jdbc.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+@Repository
+public class MyRepository {
+    private final String script = read("myScript.sql");
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public String getProductName(String name) {
+        var parameters = new HashMap<String, String>();
+        parameters.put("name", name);
+
+        var productName = namedParameterJdbcTemplate.query(
+                script, parameters, (rs, rowNum) -> rs.getString("product_name"));
+        return productName.toString();
+    }
+
+    private static String read(String scriptFileName) {
+        try (InputStream is = new ClassPathResource(scriptFileName).getInputStream();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
