eb1ae9603001619ba4de8b6e0a4ab86d935ee3f0
Dixie Dean <d.w.sokolov26@gmail.com>
2023-10-30 15:45:58 +0300

    Initial Commit

---
+++ src/main/java/com/example/homework_jdbc/HomeworkJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_jdbc/HomeworkJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
