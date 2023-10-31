020910e4ec8a9a3cc36d6b53063d1e956ef2f734
Dixie Dean <d.w.sokolov26@gmail.com>
2023-10-31 18:39:42 +0300

    MyRepository Refactored

--- src/main/java/com/example/homework_jdbc/controller/AppController.java
+++ src/main/java/com/example/homework_jdbc/controller/AppController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class AppController {
 
@@ -15,7 +17,7 @@ public class AppController {
     }
 
     @GetMapping("/products/fetch-product")
-    public String getProductName(@RequestParam String name) {
+    public List<String> getProductName(@RequestParam String name) {
         return myRepository.getProductName(name);
     }
 
--- src/main/java/com/example/homework_jdbc/repository/MyRepository.java
+++ src/main/java/com/example/homework_jdbc/repository/MyRepository.java
@@ -1,6 +1,5 @@
 package com.example.homework_jdbc.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -10,26 +9,25 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Repository
 public class MyRepository {
     private final String script = read("myScript.sql");
 
-    @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-    public String getProductName(String name) {
+    public MyRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    public List<String> getProductName(String name) {
         var parameters = new HashMap<String, String>();
         parameters.put("name", name);
-
-        var result = namedParameterJdbcTemplate.query(
-                script, parameters, (rs, rowNum) -> {
-                    var customerName = rs.getString("name");
-                    var product = rs.getString("product_name");
-                    return "Customer: " + customerName + " | " + "Product: " + product;
-                });
-        return result.toString();
+        var result =  namedParameterJdbcTemplate.queryForList(script, parameters, String.class);
+        result.forEach(System.out::println);
+        return result;
     }
 
     private static String read(String scriptFileName) {
